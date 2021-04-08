d7fdec6023c907c7300700d09964e100f32f7b1b
Raul Luis <soyrauluis@outlook.com>
2021-04-08 17:45:30 -0400

    Setup Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyBWa_3W6OgSb50hSROgkBkVMoeUDfOaa9w',
+	authDomain: 'shop-app-a3418.firebaseapp.com',
+	projectId: 'shop-app-a3418',
+	storageBucket: 'shop-app-a3418.appspot.com',
+	messagingSenderId: '551340998526',
+	appId: '1:551340998526:web:d5132f859635052eaa7d2f',
+};
