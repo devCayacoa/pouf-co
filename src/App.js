86ac25a30e87871a6de2aeebf5ae5a64dbe74788
Raul Luis <soyrauluis@outlook.com>
2021-04-08 17:55:52 -0400

    Add routes and functions for sign in, registration, recovery, etc functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React, { useReducer } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect, useReducer } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import { reducer, initialState, Context } from './store';
+
+import { Home } from './pages/Home';
+
+import { ProductsContainer } from './components/ProductsContainer';
 import { Header } from './components/Header';
 import { ProductDetailsContainer } from './components/ProductDetailsContainer';
-import { reducer, initialState, Context } from './store';
-import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { auth, handleUserProfile } from './firebase/utils';
+import { clearCurrentUser, setCurrentUser } from './store/actions';
+import { Registration } from './pages/Registration';
+import { Recovery } from './pages/Recovery';
 
 function App() {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
+	useEffect(() => {
+		let authListener = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await handleUserProfile(userAuth);
+				userRef.onSnapshot((snapshot) => {
+					dispatch(
+						setCurrentUser({
+							id: snapshot.id,
+							...snapshot.data(),
+						})
+					);
+				});
+				return;
+			}
+			dispatch(clearCurrentUser());
+		});
+
+		return () => {
+			authListener();
+		};
+	}, []);
+
 	return (
 		<Context.Provider value={{ state, dispatch }}>
 			<BrowserRouter>
-				<Header />
-				<Switch>
-					<Route exact path={'/'} component={Home} />
-					<Route
-						path={'/product/:productId'}
-						component={ProductDetailsContainer}
-					/>
-				</Switch>
+				<div id='App' className='subpixel-antialiased w-screen'>
+					<Header />
+					<main className=''>
+						<Switch>
+							<Route exact path={'/'} component={Home} />
+							<Route path='/products' component={ProductsContainer} />
+							<Route
+								path={'/product/:productId'}
+								component={ProductDetailsContainer}
+							/>
+							<Route
+								path={'/login'}
+								render={() =>
+									state.currentUser ? <Redirect to='/' /> : <Login />
+								}
+							/>
+							<Route
+								path='/registration'
+								render={() =>
+									state.currentUser ? <Redirect to='/' /> : <Registration />
+								}
+							/>
+							<Route path='/recovery' component={Recovery} />
+						</Switch>
+					</main>
+					{/* <Footer /> */}
+				</div>
 			</BrowserRouter>
 		</Context.Provider>
 	);
