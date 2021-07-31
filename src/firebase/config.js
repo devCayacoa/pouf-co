2249319efc7e426cdcb1d43fccdfbc82ee8fed32
Raul Luis <soyrauluis@outlook.com>
2021-07-30 22:55:13 -0400

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-	apiKey: 'AIzaSyBWa_3W6OgSb50hSROgkBkVMoeUDfOaa9w',
-	authDomain: 'shop-app-a3418.firebaseapp.com',
-	databaseURL: 'https://shop-app-a3418-default-rtdb.firebaseio.com',
-	projectId: 'shop-app-a3418',
-	storageBucket: 'shop-app-a3418.appspot.com',
-	messagingSenderId: '551340998526',
-	appId: '1:551340998526:web:d5132f859635052eaa7d2f',
-};
